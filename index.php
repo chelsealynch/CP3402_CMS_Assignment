cf36085c384afecc143e632d0740e9a3dd22201e
Chelsea Lynch <41936108+chelsealynch@users.noreply.github.com>
2020-04-07 12:27:37 +1000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+
+print "<!DOCTYPE html>\n";
+print "<html lang=\"en\">\n";
+print "<head>\n";
+print "    <meta charset=\"UTF-8\">\n";
+print "    <title>WeTech | Home</title>\n";
+print "    <link rel=\"stylesheet\" type=\"text/css\" href=\"startup.css\">\n";
+print "</head>\n";
+print "<body>\n";
+print "\n";
+print "<div id=\"header\">\n";
+print "    <h1>WeTech</h1>\n";
+print "    <i>Helping everyone on their journey to digital literacy, one step at a time</i>\n";
+print "</div>\n";
+print "\n";
+print "<ul>\n";
+print "    <li><a href=\"startup.html\">Home</a></li>\n";
+print "    <li><a href=\"classes.html\">Classes</a></li>\n";
+print "    <li><a href=\"timetable.html\">Timetables</a></li>\n";
+print "</ul>\n";
+print "\n";
+print "<h1>WeTech</h1>\n";
+print "<p>WeTech is dedicated to passing on technological literacy skills to older generations. Taught by experienced\n";
+print "    tutors, our classes are easy to follow and are available at convenient times and locations. Whether it's to refresh\n";
+print "    your memory, learn a new version of a program, or to simply learn the basics, we believe that no one should be left\n";
+print "    behind in the technology revolution, no matter who they are.</p>\n";
+print "\n";
+print "<img id=\"front-page-image\" src=\"https://assets.vice.com/content-images/contentimage/no-slug/cb441b23cd0b6c082d8f1a04a035833b.jpg\"\n";
+print "     alt=\"Older lady staring confused at a cellphone\"><br>\n";
+print "\n";
+print "<i>Is this you? We can help!</i>\n";
+print "\n";
+print "<p>Take a look at our startup video!</p>\n";
+print "\n";
+print "</body>\n";
+print "</html>";
+
+?>
